af1823097f4c52fcd3116026f83e836b439c8e87
Amenyuwel <SnatyGt@gmail.com>
2024-05-28 21:44:50 +0800

    camera, onboarding, pest info, kulang nalang chatbot.

--- app/src/main/java/com/example/app/HomeFragment.java
+++ app/src/main/java/com/example/app/HomeFragment.java
@@ -1,14 +1,10 @@
 package com.example.app;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -21,7 +17,6 @@ import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class HomeFragment extends Fragment {
@@ -65,7 +60,7 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.grasshopper, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.sawfly, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.slug, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.snail, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.snail_2, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.stemborer, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.weevil, ScaleTypes.FIT));
 
